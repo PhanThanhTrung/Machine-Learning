6ae7a777ae47c152e11658fc867a69ae19196e53
PhanThanhTrung <trung99nb@gmail.com>
2019-05-27 23:12:13 +0700

    sửa lại bài tập 1 để tránh chạy lại code bài 1 ở bài tập 2

--- Week 1/Ex1.py
+++ Week 1/Ex1.py
@@ -1,7 +1,7 @@
 def ExtractFileName(s):
     a=s.split('/')
     return a[-1]
-
-inputstring=input()
-#inputstring
-print(ExtractFileName(inputstring))
+if __name__=='__main__':
+    inputstring=input()
+    #inputstring
+    print(ExtractFileName(inputstring))
