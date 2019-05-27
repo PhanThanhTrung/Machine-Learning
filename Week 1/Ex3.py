622f7fb3481476e36c1e072e01e54e474f5f2890
PhanThanhTrung <trung99nb@gmail.com>
2019-05-27 14:20:48 +0700

    sửa lại private attribute và input method cho bài 3

--- Week 1/Ex3.py
+++ Week 1/Ex3.py
@@ -3,20 +3,25 @@ import json
 class Path:
     __path=''
     __file_name=''
+
+
     def __init__(self,pstring,nstring):
-        self.path=pstring
-        self.file_name=nstring
+        self.__path=pstring
+        self.__file_name=nstring
+
     def Input(self):
-        a=input('nhap path:')
-        b=input('nhap file_name:')
-        return Path(a,b)
+        self.__path=input('nhap path:')
+        self.__file_name=input('nhap file_name:')
+
     def Output(self):
-        print('path:',self.path)
-        print('file_name:', self.file_name)
+        print('path:',self.__path)
+        print('file_name:', self.__file_name)
+
     def check(self):
-        return os.path.isfile(self.path)
+        return os.path.isfile(self.__path)
+
     def ThemVaoFileJson(self):
-        my_dict={'path':self.path,'file_name':self.file_name,'exist':os.path.isfile(self.path)}
+        my_dict={'path':self.__path,'file_name':self.__file_name,'exist':os.path.isfile(self.__path)}
         with open('path.json','r') as r:
             mylist=json.load(r)
         mylist.append(my_dict)
@@ -28,10 +33,10 @@ class Path:
 #check
 my_path=Path('/home/phanthanhtrung/course-v3/nbs/dl1/00_notebook_tutorial.ipynb','00_notebook_tutorial.ipynb')
 my_path.Output()
-my_path.check()
 my_path.ThemVaoFileJson()
+
+my_path.Input()
 #test khác
 my_path=Path('/home/phanthanhtrung/course-v3/nbs/dl1/floyd_requirements.txt','floyd_requirements.txt')
 my_path.Output()
-my_path.check()
 my_path.ThemVaoFileJson()
