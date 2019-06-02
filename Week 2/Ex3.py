1efbafec861817648b9730ea72cec2c67e8267ad
PhanThanhTrung <trung99nb@gmail.com>
2019-06-02 15:20:07 +0700

    Fix #1 Ex3-week2

--- Week 2/Ex3.py
+++ Week 2/Ex3.py
@@ -1,5 +1,2 @@
 my_dict = {'a': 9, 'b': 1, 'c': 12, 'd': 7}
-
-#List={ key: value for key,value in sorted(my_dict.items(),key= lambda x: x[1])}.keys()
-#print(List)
 print(sorted(my_dict, key=my_dict.get))
